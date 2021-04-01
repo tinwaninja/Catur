923bab2f06d65558578ff2621b03d408680b7ad1
Tn. Ninja <tinwaninja@gmail.com>
2021-04-01 19:18:32 +0700

    V5

--- catur.py
+++ catur.py
@@ -175,29 +175,29 @@ def main_game(driver, engine, otomatis_main, depth, warna):
             if((warna == 'putih' and letak_gerakan % 2 == 0) or (warna == 'hitam' and letak_gerakan % 2 == 1)):
                 if mode == 'bullet':
                     if letak_gerakan <= 15:
-                        waktu = random.choice ([0.05,0.10])
+                        waktu = random.uniform(0.05,0.10)
                         print('delay', waktu,' detik')
                         time.sleep( waktu )
                     if letak_gerakan >= 15:
-                        waktu = random.choice ([0.05,0.10,0.25])
+                        waktu = random.uniform(0.05,0.25)
                         print('delay', waktu,' detik')
                         time.sleep( waktu )
                 if mode == 'blitz':
                     if letak_gerakan <= 15:
-                        waktu = random.choice ([0.05,0.10,0.25])
+                        waktu = random.uniform(0.05,0.25)
                         print('delay', waktu,' detik')
                         time.sleep( waktu )
                     if letak_gerakan >= 15:
-                        waktu = random.choice ([0.05,0.10,0.25,1.25])
+                        waktu = random.uniform(0.05,1.25)
                         print('delay', waktu,' detik')
                         time.sleep( waktu )
                 if mode == 'rapid':
                     if letak_gerakan <= 15:
-                        waktu = random.randint(0.05,0.10,0.25,1.25)
+                        waktu = random.uniform(0.05,1.25)
                         print('delay', waktu,' detik')
                         time.sleep( waktu )
                     if letak_gerakan >= 15:
-                        waktu = random.randint(0.05,0.10,0.25,1.25,1.50,1.75,2.25)
+                        waktu = random.uniform(0.05,2.25)
                         print('delay', waktu,' detik')
                         time.sleep( waktu )
                 warna_kotak(driver, terbaik)
